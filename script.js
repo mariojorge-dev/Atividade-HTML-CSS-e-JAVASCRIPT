8fe406f633c02fc73c907dfb7fbb7a26f2c0763d
mariojorge-dev <108435653+mariojorge-dev@users.noreply.github.com>
2023-11-04 17:27:46 -0300

    Finalizando

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const questions = [
     {
         question: "Qual das seguintes linguagens é usada para estruturar o conteúdo de uma página da web?",
-        correctAnswer: "HTML"
+        correctAnswer: "HTML",
+        answers: ["HTML", "CSS", "JavaScript", "XML", "C++"]
     },
     {
         question: "Qual linguagem é usada para estilizar a aparência de uma página da web?",
-        correctAnswer: "CSS"
+        correctAnswer: "CSS",
+        answers: ["HTML", "Java", "JavaScript", "Folha de Estilo em Cascatas", "CSS"]
     },
     {
         question: "Qual linguagem é usada para tornar uma página da web interativa?",
-        correctAnswer: "JavaScript"
+        correctAnswer: "JavaScript",
+        answers: ["HTML", "Python", "JavaScript", "COOKIE", "Folha de Estilo em Cascatas"]
     },
     {
         question: "O que significa a sigla 'CSS'?",
-        correctAnswer: "Folha de Estilo em Cascatas"
+        correctAnswer: "Folha de Estilo em Cascatas",
+        answers: ["HTML", " Linguagem de criação de scripts ou uma linguagem interpretada", "Folha de Estilo em Cascatas", "Linguagem de Marcação de Hipertexto", "Simplificar a seleção de fontes e aplicação de cores em páginas HTML"]
     },
     {
         question: "O que é uma closure no JavaScript?",
-        correctAnswer: "Closures são variáveis ou funções que fazem parte do escopo de uma função, possuem acesso limitado ao escopo da função a que pertencem."
+        correctAnswer: "São variáveis ou funções que fazem parte do escopo de uma função, possuem acesso limitado ao escopo da função a que pertencem.",
+        answers: ["Uma closure é um termo técnico para descrever o momento em que você fecha o navegador sem salvar seu trabalho.", "Uma closure no JavaScript é um tipo de botão que fecha automaticamente quando você clica nele.", "Uma closure é um tipo especial de variável que só pode ser usada em loops for.", "Uma closure é uma função que só pode ser chamada às segundas-feiras.", "São variáveis ou funções que fazem parte do escopo de uma função, possuem acesso limitado ao escopo da função a que pertencem."]
     }
 ];
 
@@ -26,9 +31,34 @@ let score = 0;
 
 const questionElement = document.getElementById("question");
 const scoreElement = document.getElementById("score");
+const answersContainer = document.getElementById("answers-container");
+
+function resetQuiz() {
+    currentQuestion = 0;
+    score = 0;
+    loadQuestion();
+    scoreElement.textContent = `Pontuação: ${score}`;
+}
 
 function loadQuestion() {
+    if (currentQuestion === questions.length) {
+        alert(`Quiz concluído! Sua pontuação é ${score}/${questions.length}`);
+        resetQuiz(); // Chama a função para reiniciar o quiz
+    }
+
     questionElement.textContent = questions[currentQuestion].question;
+    answersContainer.innerHTML = "";
+    questions[currentQuestion].answers.forEach(answer => {
+        const input = document.createElement("input");
+        input.type = "radio";
+        input.name = "answer";
+        input.value = answer;
+        const label = document.createElement("label");
+        label.textContent = answer;
+        answersContainer.appendChild(input);
+        answersContainer.appendChild(label);
+        answersContainer.appendChild(document.createElement("br"));
+    });
 }
 
 function checkAnswer() {
@@ -48,12 +78,11 @@ function checkAnswer() {
         loadQuestion();
     } else {
         alert(`Quiz concluído! Sua pontuação é ${score}/${questions.length}`);
-        currentQuestion = 0;
-        score = 0;
+        resetQuiz(); // Chama a função para reiniciar o quiz
     }
 
     scoreElement.textContent = `Pontuação: ${score}`;
     selectedAnswer.checked = false;
 }
 
-loadQuestion();
+resetQuiz(); // Inicia o quiz quando a página é carregada
